430b9a46fbaaf5e2c8b77caaedf1666adfb8bd3c
AVS-33 <akulasuresh102@gmail.com>
2023-05-09 15:20:10 +0530

    allfiless

---
+++ arithematic.js
@@ -0,0 +1,13 @@
+var a=10;
+var b=20;
+var c=a+b;
+var d=a-b;
+var e=a*b;
+var f=a/b;
+var g=a%b;
+
+console.log("addition is "+c);
+console.log("subtraction is "+d);
+console.log("multiplication is "+ e);
+console.log("division is "+ f);
+console.log("modulus is "+g);
